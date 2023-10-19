9c36d312fb12a5886236ba2e9cc208f7e94b1ada
ShivaKumarErpula <137309892+ShivaKumarErpula@users.noreply.github.com>
2023-10-19 12:10:37 +0530

    Add files via upload

--- a.py
+++ a.py
@@ -1,48 +1,23 @@
 import speech_recognition as sr
-from textblob import TextBlob
-import re
 
+# Initialize the recognizer
 recognizer = sr.Recognizer()
 
-def keyword_detection(text, keywords):
-    found_keywords = []
-    for keyword in keywords:
-        matches = re.findall(r'\b' + re.escape(keyword) + r'\b', text, re.IGNORECASE)
-        found_keywords.extend(matches)
-    return found_keywords
+# Use the microphone as the audio source
+with sr.Microphone() as source:
+    print("Say something:")
+    recognizer.adjust_for_ambient_noise(source)  # Adjust for ambient noise
+    audio = recognizer.listen(source)
 
-def record_audio():
-    with sr.Microphone() as source:
-        print("Please speak something...")
-        audio = recognizer.listen(source)
-    return audio
+# Recognize the speech using Google Web Speech API
+try:
+    recognized_text = recognizer.recognize_google(audio)
+    print(f"You said: {recognized_text}")
+    def calling():
+        return recognized_text
+except sr.UnknownValueError:
+    print("Google Web Speech API could not understand audio.")
+except sr.RequestError as e:
+    print(f"Could not request results from Google Web Speech API; {e}")
 
-def analyze_emotion(audio):
-    try:
-        text = recognizer.recognize_google(audio)
-        print("You said:", text)
-        found_keywords = []
-
-        sample_keywords = ["Python", "code", "programming"]
-        found_keywords = keyword_detection(text, sample_keywords)
-        print("Found Keywords:", found_keywords)
-
-        sentiment = TextBlob(text).sentiment.polarity
-        if sentiment > 0:
-            return "Happy"
-        elif sentiment < 0:
-            return "Sad"
-        else:
-            return "Neutral"
-    except sr.UnknownValueError:
-        print("Could not understand the audio")
-        return "Unable to analyze"
-    except sr.RequestError as e:
-        print(f"Error: {e}")
-        return "Request error"
-
-if __name__ == "__main__":
-    audio = record_audio()
-    emotion = analyze_emotion(audio)
-    print(f"Emotion detected: {emotion}")
-
+# You can replace the Google Web Speech API with other recognizer engines if needed.
